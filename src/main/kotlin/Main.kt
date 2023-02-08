3024e6104c88f5732f3fb7b095476bec6ac77756
joycewanjiku <joycemumbiiwanjiku@gmail.com>
2023-02-08 16:52:52 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main (){
+    greeting("Ada")
+div (24 ,12)
+    var total = addation(1,1000,120,130)
+    println(total)
+    println("clever")
+}
+fun greeting(name: String){
+    println("hello"+name)
+}
+fun div (num1:Int,num2:Int){
+    var div = num1 % num2
+    println(div)
+}
+fun  addation(num1:Int,num2:Int,num3:Int,num4:Int):Int{
+    var sum =num1+num2+num3+num4
+    return sum
+
+}
+fun fact (){
+  var myIntrest = " i love swiming"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
